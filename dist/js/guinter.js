38b1cab9d97043236fff44aaf872abf296b1905d
Guinter Weber <guinter.weber@digitaldesk.com.br>
2016-05-09 16:33:02 -0300

    correções: função das escritas, imagem de frutas

---
+++ dist/js/guinter.js
@@ -0,0 +1,24 @@
+$(document).ready(function(){
+	$.getJSON("http://192.168.1.109:8080/list", function(database){
+		var alternativas='<option value="#">Selecione uma alternativa...</option>';
+		for (var g=0; g<database.length; g++){
+			alternativas+='<option value='+database[g].chave + '>' + database[g].nome + '</option>';}
+		$("#select").html(alternativas);
+	});
+//aparecer as frutas na lista.
+	$("#botao").click(function(){
+		var z=$('#select').val();
+		$.getJSON('http://192.168.1.109:8080/product?chave=' + z, function(database)
+			{escritas(database);
+		});
+	});
+});
+//função que faz aparecer o item solicitado.
+function escritas(database){
+		var respostas='';
+		respostas+= 'Produto: ' + database.nome + '<br>';
+		respostas+= 'Valor: R$ ' + database.valor + '<br>';
+		respostas+= 'Status: ' + database.status + '<br>';
+		respostas+= 'Estoque: ' + database.estoque + '<br>';
+		$('#tudo').html(respostas);
+}

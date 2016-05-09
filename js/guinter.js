8ebe847a735710fa807a6cec27d5b79ff8510416
Guinter Weber <guinter.weber@digitaldesk.com.br>
2016-05-09 13:32:39 -0300

    icone, retirada do table

--- js/guinter.js
+++ js/guinter.js
@@ -5,18 +5,16 @@ $(document).ready(function(){
 			alternativas+='<option value='+database[g].chave + '>' + database[g].nome + '</option>';}
 		$("#select").html(alternativas);
 	});
-
+//aparecendo as frutas na lista.
 	$("#botao").click(function(){
 		var z=$('#select').val();
 		$.getJSON('http://192.168.1.109:8080/product?chave='+z, function(database){
 			var respostas='';
-			respostas+='<table border="3"><tr><th>Produto</th><th>Valor</th><th>Status</th><th>Estoque</th></tr>';
-			respostas+='<tr><td>' + database.nome + '</td>' ;
-			respostas+='<td> R$' + database.valor + '</td>';
-			respostas+='<td>' + database.status + '</td>';
-			respostas+='<td>' + database.estoque + '</td></tr></table>';
+			respostas+= 'Produto: ' + database.nome + '<br>';
+			respostas+= 'Valor: ' + 'R$' + database.valor + '<br>';
+			respostas+= 'Status: ' + database.status + '<br>';
+			respostas+= 'Estoque: ' + database.estoque + '<br>';
 			$('#tudo').html(respostas);
 		});
 	});
 });
-
